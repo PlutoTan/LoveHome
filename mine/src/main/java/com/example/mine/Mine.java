ef956c9d5d7fd39f9c4c5e7c1c4603ff7d14b1ca
fangtin19961229 <2451595858@qq.com>
2016-08-09 15:41:37 +0800

    0.001

---
+++ mine/src/main/java/com/example/mine/Mine.java
@@ -0,0 +1,13 @@
+package com.example.mine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Mine extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mine);
+    }
+}
---
+++ release/src/main/java/com/example/release/Release.java
@@ -0,0 +1,13 @@
+package com.example.release;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Release extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_release);
+    }
+}
